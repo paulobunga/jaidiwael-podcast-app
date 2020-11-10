15d992d7a24e52a4874b17c559642194ef6c6089
Jaidi Wael Leila <jaidiwael@gmail.com>
2020-11-10 10:27:47 +0100

    add others podacts

--- src/app/podcast.service.ts
+++ src/app/podcast.service.ts
@@ -5,7 +5,7 @@ import { of } from "rxjs";
   providedIn: "root",
 })
 export class PodcastService {
-  podcasts: any = [
+  podcastsTab1: any = [
     {
       url:
         "https://mcdn.podbean.com/mf/web/jnwrru/KPMG_FOT_Ep37_-_Responsible_Tax_-_v0574gnb.mp3",
@@ -14,6 +14,67 @@ export class PodcastService {
         "https://pbcdn1.podbean.com/imglogo/image-logo/3569319/Final-Tax-Podcast-Avatar.jpg",
       author: "KPMG Global Tax & Legal",
       summary: "Short podcasts addressing...",
+      color: 'blue'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/gh9ry7/FINAL-IWD-podcast.mp3",
+      name: "Future of marketing",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog3569319/IWDpic_300x300.jpg",
+      author: "KPMG Global Tax & Legal",
+      summary: "International Women's Day 2020 - Stories from notable female tax leaders",
+      color: 'red'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/q2w9pm/Future_of_Marketing_Podcast_FINAL.mp3",
+      name: "Future of Tax",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/image-logo/7700272/Customer_First_Podcast_Cover_1000px_300x300.jpg",
+      author: "Carmen Bekker, Partner, KPMG Australia",
+      summary: "This global panel discuss the rapidly shifting consumer marketplace, the rise of marketing tech and how the expectation – and role – of marketing",
+      color: 'green'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/gh9ry7/FINAL-IWD-podcast.mp3",
+      name: "Future of marketing",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog3569319/IWDpic_300x300.jpg",
+      author: "KPMG Global Tax & Legal",
+      summary: "International Women's Day 2020 - Stories from notable female tax leaders",
+      color: 'red'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/q2w9pm/Future_of_Marketing_Podcast_FINAL.mp3",
+      name: "Future of Tax",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/image-logo/7700272/Customer_First_Podcast_Cover_1000px_300x300.jpg",
+      author: "Carmen Bekker, Partner, KPMG Australia",
+      summary: "This global panel discuss the rapidly shifting consumer marketplace, the rise of marketing tech and how the expectation – and role – of marketing",
+      color: 'green'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/q2w9pm/Future_of_Marketing_Podcast_FINAL.mp3",
+      name: "Future of Tax",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/image-logo/7700272/Customer_First_Podcast_Cover_1000px_300x300.jpg",
+      author: "Carmen Bekker, Partner, KPMG Australia",
+      summary: "This global panel discuss the rapidly shifting consumer marketplace, the rise of marketing tech and how the expectation – and role – of marketing",
+      color: 'green'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/gh9ry7/FINAL-IWD-podcast.mp3",
+      name: "Future of marketing",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog3569319/IWDpic_300x300.jpg",
+      author: "KPMG Global Tax & Legal",
+      summary: "International Women's Day 2020 - Stories from notable female tax leaders",
+      color: 'red'
     },
     {
       url:
@@ -23,6 +84,7 @@ export class PodcastService {
         "https://pbcdn1.podbean.com/imglogo/image-logo/3569319/Final-Tax-Podcast-Avatar.jpg",
       author: "KPMG Global Tax & Legal",
       summary: "Short podcasts addressing...",
+      color: 'blue'
     },
     {
       url:
@@ -32,37 +94,67 @@ export class PodcastService {
         "https://pbcdn1.podbean.com/imglogo/image-logo/3569319/Final-Tax-Podcast-Avatar.jpg",
       author: "KPMG Global Tax & Legal",
       summary: "Short podcasts addressing...",
-    },
-    {
-      url:
-        "https://mcdn.podbean.com/mf/web/jnwrru/KPMG_FOT_Ep37_-_Responsible_Tax_-_v0574gnb.mp3",
-      name: "Future of Tax",
-      image:
-        "https://pbcdn1.podbean.com/imglogo/image-logo/3569319/Final-Tax-Podcast-Avatar.jpg",
-      author: "KPMG Global Tax & Legal",
-      summary: "Short podcasts addressing...",
-    },
-    {
-      url:
-        "https://mcdn.podbean.com/mf/web/jnwrru/KPMG_FOT_Ep37_-_Responsible_Tax_-_v0574gnb.mp3",
-      name: "Future of Tax",
-      image:
-        "https://pbcdn1.podbean.com/imglogo/image-logo/3569319/Final-Tax-Podcast-Avatar.jpg",
-      author: "KPMG Global Tax & Legal",
-      summary: "Short podcasts addressing...",
-    },
-    {
-      url:
-        "https://mcdn.podbean.com/mf/web/jnwrru/KPMG_FOT_Ep37_-_Responsible_Tax_-_v0574gnb.mp3",
-      name: "Future of Tax",
-      image:
-        "https://pbcdn1.podbean.com/imglogo/image-logo/3569319/Final-Tax-Podcast-Avatar.jpg",
-      author: "KPMG Global Tax & Legal",
-      summary: "Short podcasts addressing...",
-    },
+      color: 'blue'
+    }
+
   ];
 
+  podcastsTab2: any = [
+    {
+      url: "https://mcdn.podbean.com/mf/player-preload/g35x9c/04-IFRS-Today-Leases-November18_edited.mp3",
+      name: "IFRS Today",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog4225843/IFRS_Today_avatar_hi-res_with_R_mark_v2_300x300.png",
+      author: "Partner, KPMG Netherlands",
+      summary: "Preparing for IFRS 16, the new leases standard",
+      color: 'black'
+    },
+    {
+      url: "https://mcdn.podbean.com/mf/player-preload/g35x9c/04-IFRS-Today-Leases-November18_edited.mp3",
+      name: "IFRS Today",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog4225843/IFRS_Today_avatar_hi-res_with_R_mark_v2_300x300.png",
+      author: "Banking Accounting Advisory Services, KPMG in the UK",
+      summary: "Companies are finding that their interactions with crypto-assets are increasing. In this episode",
+      color: 'black'
+    }
+  ]
+
+  podcastsTab3: any = [
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/q2w9pm/Future_of_Marketing_Podcast_FINAL.mp3",
+      name: "Customer First",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/image-logo/7700272/Customer_First_Podcast_Cover_1000px_300x300.jpg",
+      author: "Insight driven engagement ",
+      summary: "we explore the concept of insight driven engagement and how organizations are getting to know their customers at a deep and profound level to help them choreograph motivational experiences.",
+      color: 'yellow'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/sk9rd4/Future_of_Service_podcast_FINAL6v7fx.mp3",
+      name: "Customer First",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/image-logo/7700272/Customer_First_Podcast_Cover_1000px_300x300.jpg",
+      author: "Insight driven engagement ",
+      summary: "we explore the concept of insight driven engagement and how organizations are getting to know their customers at a deep and profound level to help them choreograph motivational experiences.",
+      color: 'yellow'
+    },
+    {
+      url:
+        "https://mcdn.podbean.com/mf/player-preload/b27ih3/Value_of_CX_Podcast_FINALax4dg.mp3",
+      name: "Value of customer experience (CX)",
+      image:
+        "https://pbcdn1.podbean.com/imglogo/image-logo/7700272/Customer_First_Podcast_Cover_1000px_300x300.jpg",
+      author: "Insight driven engagement ",
+      summary: "This episode of the Customer First podcast brings together a panel from Asia, Europe and North America to examine the true value of customer experience (CX). ",
+      color: 'yellow'
+    }
+  ]
+
   getPodcasts() {
-    return of(this.podcasts);
+    return of(this.podcastsTab1);
   }
+
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,140 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
+import { Store } from "@ngrx/store";
+import {
+  CANPLAY,
+  GET_PODCASTS,
+  LOADEDMETADATA,
+  LOADSTART,
+  PLAYING,
+  RESET,
+  SET_CURRENT_TRACK,
+  TIMEUPDATE,
+} from "src/store";
+import { AudioService } from "./../audio.service";
+import { PodcastService } from "./../podcast.service";
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements AfterViewInit {
+  podcasts: any = [];
+  currentPodcast: any = {};
+  state: any = {};
 
-  constructor() {}
+  constructor(
+    private podcastService: PodcastService,
+    private audioService: AudioService,
+    private store: Store<any>,
+    public loadingCtrl: LoadingController
+  ) {
+    this.getPodcasts();
+  }
+  ngAfterViewInit(): void {
+    this.store.select("appState").subscribe((value) => {
+      if (value) {
+        this.podcasts = value.podcasts;
+        if (value.currentPodcast) {
+          this.currentPodcast = value.currentPodcast;
+        }
+      }
+    });
+  }
 
+  openPodcast(podcast, index) {
+    let currentPodcast = { index, podcast };
+    this.store.dispatch({
+      type: SET_CURRENT_TRACK,
+      payload: { value: currentPodcast },
+    });
+    this.playStream(podcast.url);
+  }
+
+  resetState() {
+    this.audioService.stop();
+    this.store.dispatch({ type: RESET });
+  }
+
+  playStream(url) {
+    this.resetState();
+    this.audioService.playStream(url).subscribe((event) => {
+      const audioObj = event.target;
+
+      switch (event.type) {
+        case "canplay":
+          return this.store.dispatch({
+            type: CANPLAY,
+            payload: { value: true },
+          });
+
+        case "loadedmetadata":
+          return this.store.dispatch({
+            type: LOADEDMETADATA,
+            payload: {
+              value: true,
+              data: {
+                time: this.audioService.formatTime(
+                  audioObj.duration * 1000,
+                  "HH:mm:ss"
+                ),
+                timeSec: audioObj.duration,
+                mediaType: "mp3",
+              },
+            },
+          });
+
+        case "playing":
+          return this.store.dispatch({
+            type: PLAYING,
+            payload: { value: true },
+          });
+
+        case "pause":
+          return this.store.dispatch({
+            type: PLAYING,
+            payload: { value: false },
+          });
+
+        case "timeupdate":
+          return this.store.dispatch({
+            type: TIMEUPDATE,
+            payload: {
+              timeSec: audioObj.currentTime,
+              time: this.audioService.formatTime(
+                audioObj.currentTime * 1000,
+                "HH:mm:ss"
+              ),
+            },
+          });
+
+        case "loadstart":
+          return this.store.dispatch({
+            type: LOADSTART,
+            payload: { value: true },
+          });
+      }
+    });
+  }
+
+  async getPodcasts() {
+    let loader = await this.presentLoader();
+    this.podcastService.getPodcasts().subscribe((podcasts) => {
+      //this.podcasts = podcasts;
+      this.store.dispatch({
+        type: GET_PODCASTS,
+        payload: { value: podcasts },
+      });
+      loader.dismiss();
+    });
+  }
+
+  async presentLoader() {
+    const loading = await this.loadingCtrl.create({
+      message: "Loading podcasts. Please wait...",
+      duration: 2000,
+    });
+    await loading.present();
+    return loading;
+  }
 }
